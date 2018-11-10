8c144594bdf502c26c29781f13d429c978798ab0
Stefan Trauth <mail@stefantrauth.de>
2018-11-10 12:07:42 +0100

    create api calls to load embed content and playlist

--- Sources/Core/PodigeeEmbedKit.swift
+++ Sources/Core/PodigeeEmbedKit.swift
@@ -8,8 +8,59 @@
 
 import Foundation
 
-class PodigeeEmbedKit {
+public class PodigeeEmbedKit {
     
-    static let name = "PodigeeEmbedKit"
+    private static var jsonDecoder: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        return decoder
+    }
+    
+    public static func embedDataForPodcastWith(domain: String, complete: @escaping (_ embed: PodcastEmbed?, _ error: Error?) -> Void) {
+        var components = URLComponents()
+        components.host = domain
+        components.path = "embed"
+        components.queryItems = [URLQueryItem(name: "context", value: "external")]
+        components.scheme = "https"
+        
+        guard let url = components.url else {
+            #warning("return invalid domaint error")
+            return
+        }
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data, let embed = try? jsonDecoder.decode(PodcastEmbed.self, from: data) else {
+                complete(nil, error)
+                return
+            }
+            complete(embed, nil)
+        }.resume()
+    }
+    
+    public static func playlistForPodcastWith(domain: String, complete: @escaping (_ episodes: Episodes?, _ error: Error?) -> Void) {
+        var components = URLComponents()
+        components.host = domain
+        components.path = "embed/playlist"
+        components.queryItems = [URLQueryItem(name: "context", value: "external")]
+        components.scheme = "https"
+        
+        guard let url = components.url else {
+            #warning("return invalid domaint error")
+            return
+        }
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data, let embed = try? jsonDecoder.decode(Episodes.self, from: data) else {
+                complete(nil, error)
+                return
+            }
+            complete(embed, nil)
+            }.resume()
+    }
+    
     
 }

9bc5a2114cad5f6bfe8d0e0c06b9abf6e43d4858
Stefan Trauth <mail@stefantrauth.de>
2018-11-15 10:49:15 +0100

    stub network requests when running test suite

--- Tests/PodigeeEmbedKitSpec.swift
+++ Tests/PodigeeEmbedKitSpec.swift
@@ -8,18 +8,28 @@
 
 import Quick
 import Nimble
+import OHHTTPStubs
 @testable import PodigeeEmbedKit
 
 class PodigeeEmbedKitSpec: QuickSpec {
 
     override func spec() {
         describe("embed data") {
+            
+            stub(condition: isHost("podcast-news.podigee.io"), response: { (request) -> OHHTTPStubsResponse in
+                return OHHTTPStubsResponse(
+                    fileAtPath: OHPathForFile("podcast_news.json", type(of: self))!,
+                    statusCode: 200,
+                    headers: ["Content-Type":"application/json"]
+                )
+            })
+            
             it("returns the correct podcast title") {
                 var podcastTitle: String?
-                PodigeeEmbedKit.embedDataForPodcastWith(domain: "bananaland.podigee.io") { (podcastEmbed, error) in
+                PodigeeEmbedKit.embedDataForPodcastWith(domain: "podcast-news.podigee.io") { (podcastEmbed, error) in
                     podcastTitle = podcastEmbed?.podcast.title
                 }
-                expect(podcastTitle).toEventually(equal("Banana Land"))
+                expect(podcastTitle).toEventually(equal("Podigee Podcast News"))
             }
         }
 
